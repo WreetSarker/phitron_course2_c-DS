4c066e4bee23d429a1a3f57168145e7c95dea1cc
WreetSarker <wreet.sarker@gmail.com>
2022-08-20 19:14:54 +0600

    module 24 completed

--- module_24/prb10.cpp
+++ module_24/prb10.cpp
@@ -1,11 +1,99 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+template<typename N>
+class Node{
+public:
+    N data;
+    Node* next;
+    Node* prev;
+
+    Node(N data){
+        this->data = data;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
 
 
+template<typename S>
+class Stack{
+public:
+Node<S>* head;
+Node<S>* top;
+int size;
+
+    Stack(){
+        this->head = NULL;
+        this->top = NULL;
+        this->size = 0;
+    }
+
+    void push(S val){
+        Node<S>* newNode = new Node<S>(val);
+        if(head == NULL){
+            head = newNode;
+            top = newNode;
+            this->size++;
+            return;
+        }
+        top->next = newNode;
+        newNode->prev = top;
+        top = newNode;
+        this->size++;
+    }
+
+    S pop(){
+        S chck;
+        if(head == NULL){
+            cout<<"Empty stack!"<<endl;
+            return chck;
+        }
+        if(this->size == 1){
+            S retVal = top->data;
+            this->head = NULL;
+            this->top = NULL;
+            this->size--;
+            return retVal;
+        }
+        Node<S>* delNode = top;
+        top = delNode->prev;
+        top->next = NULL;
+        S returnVal = delNode->data;
+        this->size--;
+        delete delNode;
+        return returnVal;
+    }
+
+    bool isEmpty(){
+        return this->size == 0;
+    }
+
+    int getSize(){
+        return this->size;
+    }
+
+    S getTop(){
+        S topVal = top->data;
+        return topVal;
+    }
+
+    S mid(){
+        if(this->getSize() == 0){
+            cout<<"Empty Stack!"<<endl;
+            return -1;
+        }
+        Node<S>* slowPtr = head;
+        Node<S>* fastPtr = head;
+
+        while(fastPtr!=NULL && fastPtr->next != NULL){
+            slowPtr = slowPtr->next;
+            fastPtr = fastPtr->next->next;
+        }
+        return slowPtr->data;
+    }
 
 
+};
 
-    return 0;
-}
+
--- module_24/prb9.cpp
+++ module_24/prb9.cpp
@@ -1,11 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int n,left_arr[105],right_arr[105];
+int st[105],ed[105],cnt,ct2;
+
+int change(int x){
+	while(right_arr[x]) x=right_arr[x];
+	return x;
+}
+
 int main(){
-
-
-
-
-
-    return 0;
+	int n;
+	cin>>n;
+	for(int i=1;i<=n;i++) {
+        cin>>left_arr[i]>>right_arr[i];
+	}
+	for(int i=1;i<=n;i++) {
+		if(left_arr[i]==0){
+			st[++cnt]=i;
+			ed[cnt]=change(i);
+		}
+	}
+	for(int i=1;i<cnt;i++) {
+		right_arr[ed[i]]=st[i+1];
+		left_arr[st[i+1]]=ed[i];
+	}
+	for(int i=1;i<=n;i++){
+        cout<<left_arr[i]<<" "<<right_arr[i]<<endl;
+	}
 }
