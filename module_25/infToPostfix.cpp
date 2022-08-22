0e5caf6799dbc4a3ac556ef4e21f27c0adfc4bcd
WreetSarker <wreet.sarker@gmail.com>
2022-08-22 13:48:37 +0600

    module 25 added

---
+++ module_25/infToPostfix.cpp
@@ -0,0 +1,95 @@
+#include<bits/stdc++.h>
+#include"MY_STACK.h"
+
+using namespace std;
+
+int precedenceCalc(char c){
+    if(c == '^'){
+        return 3;
+    }else if(c == '*' || c == '/'){
+        return 2;
+    }else if(c == '+' || c == '-'){
+        return 1;
+    }else{
+        return -1;
+    }
+}
+
+
+string infixToPostfix(string chk){
+    Stack<char> st;
+    string result = "";
+
+    for(int i = 0; i < chk.size(); i++){
+        if(chk[i]>= '0' && chk[i]<= '9'){
+            result+= chk[i];
+        }else if(chk[i] == '('){
+            st.push(chk[i]);
+        }else if(chk[i] == ')'){
+            while(!st.isEmpty() && st.getTop() != '('){
+                result+=st.pop();
+            }
+            if(!st.isEmpty()) st.pop();
+        }else{
+            while(!st.isEmpty() && precedenceCalc(st.getTop()) > precedenceCalc(chk[i])){
+                result +=st.pop();
+            }
+            st.push(chk[i]);
+        }
+    }
+    while(!st.isEmpty()){
+        result += st.pop();
+    }
+
+    return result;
+}
+
+
+int postfixEvaluation(string chk){
+    Stack<int> st;
+
+    for(int i = 0; i < chk.size(); i++){
+        if(chk[i]>= '0' && chk[i] <= '9'){
+            st.push(chk[i] - '0');
+        }else{
+            int a = st.pop();
+            int b = st.pop();
+            switch(chk[i]){
+            case '+':
+                st.push(a+b);
+                break;
+            case '-':
+                st.push(a-b);
+                break;
+            case '*':
+                st.push(a*b);
+                break;
+            case '/':
+                st.push(a/b);
+                break;
+            case '^':
+                st.push(pow(a, b));
+                break;
+            default:
+                break;
+            }
+        }
+    }
+    return st.getTop();
+}
+
+int main(){
+
+    string s;
+    cout<<"Enter string to evaluate: ";
+    cin>>s;
+    string res;
+    res = infixToPostfix(s);
+    cout<<"Prefix is: "<<res<<endl;
+    cout<<"Result is: "<<postfixEvaluation(res)<<endl;
+
+
+
+
+    return 0;
+}
